f8ec9e46e8cf023518f6da3c7e22c50c100d6d1d
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-11-02 01:18:10 +0530

    factorial code wprocessed succesfully

---
+++ Single_cycle/test_factorial__.asm
@@ -0,0 +1,14 @@
+#  t5 -> cumulative product
+#  t2 -> N
+
+
+
+addi    $t5, $t5, 1
+for:
+	beq $t0,$zero endFor
+	mul $t5,$t0,$t5
+	addi $t0,$t0,-1
+	beq $0, $0, for
+endFor:
+
+	sw $t5,0($t2)
---
+++ Single_cycle/test_sort__.asm
@@ -0,0 +1,44 @@
+# s0 -> i
+# s1 -> j
+
+# t3 => maximum number of comparisons needed
+# i.e  j -> [0 to t3)
+
+
+addi    $s0, $0	, 0		            # i = 0
+addi	$t5, $t2, 0			        # t5  <- temp_t2 
+
+outer_loop__:
+    addi    $s0, $s0, 1             # i++
+    addi    $t2, $t5, 0             # restoring t2 back to start of array
+
+
+    beq     $s0, $t0, print_sorted_loop_init__
+
+    # inner loop initialisation
+    addi    $s1, $0	, 0		        # j = 0
+    sub		$t3, $t0, $s0		    # $t3 <- N - i    ( you can do this using one register)
+
+    inner_loop__:
+    beq     $s1, $t3, outer_loop__  # j == N - i go to outer lopp
+
+    lw      $s3, 0($t2)             # s3 <- M[ t2 ]
+    lw      $s4, 4($t2)             # s4 <- M[t2+4]
+
+    # Only if A[j] > A[j+1] we'll swap
+    slt		$t1, $s4, $s3		    # $t1 = ($s3 < $s4) ? 1 : 0
+    beq     $t1, $0, no_swap        # j == N - i go to outer lopp
+    
+
+    # swapping
+    sw		$s3, 4($t2)		        # s3 -> M[t2+4]
+    sw		$s4, 0($t2)		        # s4 -> M[ t2 ] 
+
+    no_swap:
+    addi     $s1, $s1, 1            # j++
+    addi     $t2, $t2, 4            # t2 += 4   
+    beq		 $0 , $0 , inner_loop__				# next iteration of inner loop
+
+
+print_sorted_loop_init__:
+addi    $s1, $0	, 0		            # j = 0
